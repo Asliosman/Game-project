f4db209a23e962fbec149a800a2499d65fe2fad2
Asli osman <Aslim@metropolia.fi>
2026-09-02 11:14:19 +0300

    modified Alien attack game

--- .project.py
+++ .project.py
@@ -1,4 +1,25 @@
 player=(input("what is the player`s name"))
-age=(input("what is the player`s age"))
+age=int(input("what is the player`s age"))
 print(player)
 print(age)
+if age<12:
+    print("minor;the game is shutting down")
+else:
+    print("hello")
+while True:
+    print("main menu")
+    command=input("enter command")
+    
+    if command=="lopeta":
+        print("game ended")
+        break
+    elif command=="explore":
+        print("you explore the allien planet")
+    elif command=="fight":
+        print("you fight an allien")
+    elif command=="treasure":
+        print("you found an allien treasure")
+    else:
+        print("unknown command")
+
+
