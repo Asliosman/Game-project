9bb3c486bbe88cd04e02551372c9d3fc1ffaa2e9
Asli osman <Aslim@metropolia.fi>
2026-09-08 19:30:51 +0300

    added functions

--- .project.py
+++ .project.py
@@ -1,3 +1,14 @@
+discoveries=[]
+def explore():
+    place=input("enter a place to explore")
+    discoveries.append(place)
+    print("you explore:",place)
+def show_discoverie():
+    print("your discoveries:")
+    for place in discoveries:
+        print(place)
+def fight():
+    print("you fight an allien!")
 player=(input("what is the player`s name"))
 age=int(input("what is the player`s age"))
 print(player)
@@ -8,17 +19,24 @@ else:
     print("hello")
 while True:
     print("main menu")
+    print("explore")
+    print("fight")
+    print("treasure")
+    print("show")
+    print("lopeta")
     command=input("enter command")
     
     if command=="lopeta":
         print("game ended")
         break
     elif command=="explore":
-        print("you explore the allien planet")
+        explore()
     elif command=="fight":
-        print("you fight an allien")
+        fight()
     elif command=="treasure":
         print("you found an allien treasure")
+    elif command=="show":
+        show_discoveries()
     else:
         print("unknown command")
 
